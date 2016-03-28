dd21301fb1d503ea23c14fa987af6753dfe5ac1e
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-03-28 12:47:42 +0100

    make toggle actions DumbAware

--- fridaymario/ToggleActionSounds.java
+++ fridaymario/ToggleActionSounds.java
@@ -2,8 +2,9 @@ package fridaymario;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.ToggleAction;
+import com.intellij.openapi.project.DumbAware;
 
-public class ToggleActionSounds extends ToggleAction {
+public class ToggleActionSounds extends ToggleAction implements DumbAware {
 	@Override public boolean isSelected(AnActionEvent anActionEvent) {
 		return Settings.getInstance().actionSoundsEnabled;
 	}
--- fridaymario/ToggleBackgroundMusic.java
+++ fridaymario/ToggleBackgroundMusic.java
@@ -2,8 +2,9 @@ package fridaymario;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.ToggleAction;
+import com.intellij.openapi.project.DumbAware;
 
-public class ToggleBackgroundMusic extends ToggleAction {
+public class ToggleBackgroundMusic extends ToggleAction implements DumbAware {
 	@Override public boolean isSelected(AnActionEvent anActionEvent) {
 		return Settings.getInstance().backgroundMusicEnabled;
 	}
