bc8ad1e78317380cac8b0f9f3467e9b039d14332
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-04-18 15:40:57 +0100

    don't use deprecated API

--- src/fridaymario/listeners/AllActions.java
+++ src/fridaymario/listeners/AllActions.java
@@ -6,23 +6,25 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.ex.AnActionListener;
+import com.intellij.openapi.application.ApplicationManager;
 import org.jetbrains.annotations.NotNull;
 
 public class AllActions implements Restartable {
-	private final AnActionListener actionListener;
+	private final Listener listener;
 
 	public AllActions(final Listener listener) {
-		actionListener = new AnActionListener() {
-			@Override public void beforeActionPerformed(@NotNull AnAction action,
-			                                            @NotNull DataContext dataContext,
-			                                            @NotNull AnActionEvent event) {
-				listener.onAction(ActionManager.getInstance().getId(action));
-			}
-		};
+		this.listener = listener;
 	}
 
 	@Override public void start(Disposable disposable) {
-		ActionManager.getInstance().addAnActionListener(actionListener, disposable);
+		ApplicationManager.getApplication().getMessageBus().connect(disposable)
+				.subscribe(AnActionListener.TOPIC, new AnActionListener() {
+					@Override public void beforeActionPerformed(@NotNull AnAction action,
+					                                            @NotNull DataContext dataContext,
+					                                            @NotNull AnActionEvent event) {
+						listener.onAction(ActionManager.getInstance().getId(action));
+					}
+				});
 	}
 
 	public interface Listener {
