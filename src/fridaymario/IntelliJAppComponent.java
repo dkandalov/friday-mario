fbfb1df5a4318df61238bec5f43a982a195e59a5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-04-18 13:52:41 +0100

    use Disposable instead of Restartable.stop(); removed redundant code

--- src/fridaymario/IntelliJAppComponent.java
+++ src/fridaymario/IntelliJAppComponent.java
@@ -4,56 +4,46 @@ import com.intellij.ide.AppLifecycleListener;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
-import com.intellij.openapi.application.Application;
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectManagerListener;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.SystemInfo;
 import fridaymario.listeners.*;
 import fridaymario.sounds.SilentSound;
 import fridaymario.sounds.Sounds;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static com.intellij.openapi.util.text.StringUtilRt.isEmptyOrSpaces;
 
-public class IntelliJAppComponent {
-	private final Map<Project, Refactoring> refactoringByProject = new HashMap<>();
-	private final Map<Project, VcsActions> vcsActionsByProject = new HashMap<>();
-	private final Map<Project, Compilation> compilationByProject = new HashMap<>();
-	private final Map<Project, UnitTests> unitTestsByProject = new HashMap<>();
-
+public class IntelliJAppComponent implements AppLifecycleListener {
 	private ActionListeningSoundPlayer soundPlayer;
-	private AllActions allActions;
-
-	private ProjectManagerListener projectManagerListener;
+	private Disposable disposable;
 	private boolean silentMode;
 	private boolean logUnmappedActions;
 
-	public IntelliJAppComponent() {
-		Application application = ApplicationManager.getApplication();
-		application.getMessageBus().connect().subscribe(AppLifecycleListener.TOPIC, new AppLifecycleListener() {
-			@Override public void appFrameCreated(@NotNull List<String> commandLineArgs) {
-				if (!Settings.getInstance().isPluginEnabled()) return;
-				init();
-			}
+	@Override public void appFrameCreated(@NotNull List<String> commandLineArgs) {
+		if (!Settings.getInstance().isPluginEnabled()) return;
+		init();
+	}
 
-			@Override public void appWillBeClosed(boolean isRestart) {
-				if (!Settings.getInstance().isPluginEnabled()) return;
-				soundPlayer.stopAndPlayGameOver();
-				dispose(true);
-			}
-		});
+	@Override public void appWillBeClosed(boolean isRestart) {
+		if (!Settings.getInstance().isPluginEnabled()) return;
+		soundPlayer.stopAndPlayGameOver();
+		dispose(true);
 	}
 
 	private void init() {
+		disposable = Disposer.newDisposable("FridayMario");
+		Disposer.register(ApplicationManager.getApplication(), disposable);
+
 		soundPlayer = new ActionListeningSoundPlayer(createSounds(), createLoggingListener()).init();
-		initApplicationListeners();
-		initProjectListeners();
+		initApplicationListeners(disposable);
+		initProjectListeners(disposable);
 
 		// see https://github.com/dkandalov/friday-mario/issues/3#issuecomment-160421286
 		// and http://keithp.com/blogs/Java-Sound-on-Linux/
@@ -66,83 +56,31 @@ public class IntelliJAppComponent {
 
 	private void dispose(boolean isIdeShutdown) {
 		if (soundPlayer == null) return;
-
-		disposeProjectListeners();
-		disposeApplicationListeners();
+		disposable.dispose();
 		soundPlayer.stop(isIdeShutdown);
-
 		soundPlayer = null;
 	}
 
-	private void initApplicationListeners() {
-		allActions = new AllActions(soundPlayer);
-		allActions.start(ApplicationManager.getApplication());
+	private void initApplicationListeners(Disposable disposable) {
+		new AllActions(soundPlayer).start(disposable);
 	}
 
-	private void disposeApplicationListeners() {
-		allActions.stop();
-	}
-
-	private void initProjectListeners() {
-		projectManagerListener = new ProjectManagerListener() {
+	private void initProjectListeners(Disposable disposable) {
+		ProjectManagerListener projectManagerListener = new ProjectManagerListener() {
 			@Override public void projectOpened(@NotNull Project project) {
-				if (!refactoringByProject.containsKey(project)) {
-					Refactoring refactoring = new Refactoring(project, soundPlayer);
-					refactoring.start(project);
-					refactoringByProject.put(project, refactoring);
-				}
-
-				if (!vcsActionsByProject.containsKey(project)) {
-					VcsActions vcsActions = new VcsActions(project, soundPlayer);
-					vcsActions.start(project);
-					vcsActionsByProject.put(project, vcsActions);
-				}
-
-				if (!compilationByProject.containsKey(project) && isIdeWithCompilation()) {
-					Compilation compilation = Compilation.factory.create(project, soundPlayer);
-					compilation.start(project);
-					compilationByProject.put(project, compilation);
-				}
-
-				if (!unitTestsByProject.containsKey(project)) {
-					UnitTests unitTests = new UnitTests(project, soundPlayer);
-					unitTests.start(project);
-					unitTestsByProject.put(project, unitTests);
-				}
-			}
-
-			@Override public void projectClosed(@NotNull Project project) {
-				if (refactoringByProject.containsKey(project)) {
-					refactoringByProject.get(project).stop();
-					refactoringByProject.remove(project);
-				}
-				if (vcsActionsByProject.containsKey(project)) {
-					vcsActionsByProject.get(project).stop();
-					vcsActionsByProject.remove(project);
-				}
-				if (compilationByProject.containsKey(project)) {
-					compilationByProject.get(project).stop();
-					compilationByProject.remove(project);
-				}
-				if (unitTestsByProject.containsKey(project)) {
-					unitTestsByProject.get(project).stop();
-					unitTestsByProject.remove(project);
-				}
+				new Refactoring(project, soundPlayer).start(project);
+				new VcsActions(project, soundPlayer).start(project);
+				Compilation.factory.create(project, soundPlayer).start(project);
+				new UnitTests(project, soundPlayer).start(project);
 			}
 		};
 
+		// TODO is this necessary?
 		for (Project project : ProjectManager.getInstance().getOpenProjects()) {
 			projectManagerListener.projectOpened(project);
 		}
-		ProjectManager.getInstance().addProjectManagerListener(projectManagerListener);
-	}
 
-	private void disposeProjectListeners() {
-		ProjectManager projectManager = ProjectManager.getInstance();
-		for (Project project : projectManager.getOpenProjects()) {
-			projectManagerListener.projectClosed(project);
-		}
-		projectManager.removeProjectManagerListener(projectManagerListener);
+		ProjectManager.getInstance().addProjectManagerListener(projectManagerListener, disposable);
 	}
 
 	public IntelliJAppComponent silentMode() {
@@ -158,8 +96,13 @@ public class IntelliJAppComponent {
 	private Sounds createSounds() {
 		if (silentMode) {
 			return Sounds.createSilent(new SilentSound.Listener() {
-				@Override public void playing(String soundName) { show(soundName); }
-				@Override public void stopped(String soundName) { show("stopped: " + soundName); }
+				@Override public void playing(String soundName) {
+					show(soundName);
+				}
+
+				@Override public void stopped(String soundName) {
+					show("stopped: " + soundName);
+				}
 			});
 		} else {
 			Settings settings = Settings.getInstance();
@@ -190,15 +133,6 @@ public class IntelliJAppComponent {
 		return ApplicationManager.getApplication().getComponent(IntelliJAppComponent.class);
 	}
 
-	private static boolean isIdeWithCompilation() {
-		try {
-			Class.forName("com.intellij.openapi.compiler.CompilationStatusAdapter");
-			return true;
-		} catch (ClassNotFoundException e) {
-			return false;
-		}
-	}
-
 	public void setBackgroundMusicEnabled(boolean value) {
 		Settings.getInstance().setBackgroundMusicEnabled(value);
 		update();
@@ -210,11 +144,8 @@ public class IntelliJAppComponent {
 	}
 
 	private void update() {
-		Settings settings = Settings.getInstance();
-		if (!settings.actionSoundsEnabled && !settings.backgroundMusicEnabled) {
-			dispose(false);
-		} else {
-			dispose(false);
+		dispose(false);
+		if (Settings.getInstance().isPluginEnabled()) {
 			init();
 		}
 	}
