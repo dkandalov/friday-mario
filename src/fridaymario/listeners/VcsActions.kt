a0cdf558fdaa46a42bec098897480e6d22b1ad48
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-04-19 21:21:22 +0100

    java to kotlin

--- src/fridaymario/listeners/VcsActions.kt
+++ src/fridaymario/listeners/VcsActions.kt
@@ -1,95 +1,75 @@
-package fridaymario.listeners;
+package fridaymario.listeners
 
-import com.intellij.notification.Notification;
-import com.intellij.notification.Notifications;
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.vcs.CheckinProjectPanel;
-import com.intellij.openapi.vcs.changes.CommitContext;
-import com.intellij.openapi.vcs.checkin.CheckinHandler;
-import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory;
-import com.intellij.openapi.vcs.impl.CheckinHandlersManager;
-import com.intellij.openapi.vcs.update.UpdatedFilesListener;
-import com.intellij.util.messages.MessageBus;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.notification.Notification
+import com.intellij.notification.Notifications
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vcs.CheckinProjectPanel
+import com.intellij.openapi.vcs.changes.CommitContext
+import com.intellij.openapi.vcs.checkin.CheckinHandler
+import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory
+import com.intellij.openapi.vcs.update.UpdatedFilesListener
 
-public class VcsActions implements Restartable {
-	private final UpdatedFilesListener updatedListener;
-	private final Notifications pushListener;
-	private final Listener listener;
-	private final MessageBus messageBus;
+class VcsActions(private val project: Project, private val listener: Listener): Restartable {
+    private val updatedListener = UpdatedFilesListener { listener.onVcsUpdate() }
 
-	public VcsActions(Project project, final Listener listener) {
-		this.messageBus = project.getMessageBus();
+    // see git4idea.push.GitPushResultNotification#create
+    // see org.zmlx.hg4idea.push.HgPusher#push
+    private val pushListener: Notifications = object: Notifications {
+        override fun notify(notification: Notification) {
+            if (!isVcsNotification(notification)) return
+            if (matchTitleOf(notification, "Push successful")) {
+                listener.onVcsPush()
+            } else if (matchTitleOf(notification, "Push failed", "Push partially failed", "Push rejected", "Push partially rejected")) {
+                listener.onVcsPushFailed()
+            }
+        }
+    }
 
-		updatedListener = files -> listener.onVcsUpdate();
+    override fun start(disposable: Disposable) {
+        // using bus to listen to vcs updates because normal listener calls it twice
+        // (see also https://gist.github.com/dkandalov/8840509)
+        val messageBus = project.messageBus
+        messageBus.connect(disposable).subscribe(UpdatedFilesListener.UPDATED_FILES, updatedListener)
+        messageBus.connect(disposable).subscribe(Notifications.TOPIC, pushListener)
+        MyCheckinHandlerFactory.listener = listener
+        Disposer.register(disposable, Disposable { MyCheckinHandlerFactory.listener = null })
+    }
 
-		// see git4idea.push.GitPushResultNotification#create
-		// see org.zmlx.hg4idea.push.HgPusher#push
-		pushListener = new Notifications() {
-			@Override public void notify(@NotNull Notification notification) {
-				if (!isVcsNotification(notification)) return;
-				if (matchTitleOf(notification, "Push successful")) {
-					listener.onVcsPush();
-				} else if (matchTitleOf(notification, "Push failed", "Push partially failed", "Push rejected", "Push partially rejected")) {
-					listener.onVcsPushFailed();
-				}
-			}
-		};
-		this.listener = listener;
-	}
+    /**
+     * Using listener registration through extension point even though it's not reloadable.
+     * This is because deprecated part of CheckinHandlersManager API was deleted in IJ15.
+     */
+    class MyCheckinHandlerFactory: CheckinHandlerFactory() {
+        override fun createHandler(checkinProjectPanel: CheckinProjectPanel, commitContext: CommitContext) =
+            object: CheckinHandler() {
+                override fun checkinSuccessful() {
+                    if (listener != null) listener!!.onVcsCommit()
+                }
+            }
 
-	@Override public void start(Disposable disposable) {
-		// using bus to listen to vcs updates because normal listener calls it twice
-		// (see also https://gist.github.com/dkandalov/8840509)
-		messageBus.connect(disposable).subscribe(UpdatedFilesListener.UPDATED_FILES, updatedListener);
-		messageBus.connect(disposable).subscribe(Notifications.TOPIC, pushListener);
-		MyCheckinHandlerFactory.listener = listener;
-		Disposer.register(disposable, () -> MyCheckinHandlerFactory.listener = null);
-	}
+        companion object {
+            var listener: Listener? = null
+        }
+    }
 
-	private static boolean isVcsNotification(Notification notification) {
-		return notification.getGroupId().equals("Vcs Messages") ||
-				notification.getGroupId().equals("Vcs Important Messages") ||
-				notification.getGroupId().equals("Vcs Minor Notifications") ||
-				notification.getGroupId().equals("Vcs Silent Notifications");
-	}
+    interface Listener {
+        fun onVcsCommit()
+        fun onVcsUpdate()
+        fun onVcsPush()
+        fun onVcsPushFailed()
+    }
 
-	private static boolean matchTitleOf(Notification notification, String... expectedTitles) {
-		for (String title : expectedTitles) {
-			if (notification.getTitle().startsWith(title)) return true;
-		}
-		return false;
-	}
+    companion object {
+        private fun isVcsNotification(notification: Notification) =
+            notification.groupId == "Vcs Messages"
+                || notification.groupId == "Vcs Important Messages"
+                || notification.groupId == "Vcs Minor Notifications"
+                || notification.groupId == "Vcs Silent Notifications"
 
-	/**
-	 * Using listener registration through extension point even though it's not reloadable.
-	 * This is because deprecated part of {@link CheckinHandlersManager} API was deleted in IJ15.
-	 */
-	public static class MyCheckinHandlerFactory extends CheckinHandlerFactory {
-		public static Listener listener;
-
-		@NotNull @Override
-		public CheckinHandler createHandler(@NotNull CheckinProjectPanel checkinProjectPanel,
-		                                    @NotNull CommitContext commitContext) {
-			return new CheckinHandler() {
-				@Override public void checkinSuccessful() {
-					if (listener != null) {
-						listener.onVcsCommit();
-					}
-				}
-			};
-		}
-	}
-
-	public interface Listener {
-		void onVcsCommit();
-
-		void onVcsUpdate();
-
-		void onVcsPush();
-
-		void onVcsPushFailed();
-	}
+        private fun matchTitleOf(notification: Notification, vararg expectedTitles: String): Boolean {
+            return expectedTitles.any { notification.title.startsWith(it) }
+        }
+    }
 }
