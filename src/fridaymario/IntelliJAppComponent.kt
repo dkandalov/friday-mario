8f18ffa3f869e50190cd257eea5081384f1f64a0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-04-19 12:09:10 +0100

    java to kotlin

--- src/fridaymario/IntelliJAppComponent.kt
+++ src/fridaymario/IntelliJAppComponent.kt
@@ -1,153 +1,146 @@
-package fridaymario;
+package fridaymario
 
-import com.intellij.ide.AppLifecycleListener;
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.project.ProjectManagerListener;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.util.SystemInfo;
-import fridaymario.listeners.*;
-import fridaymario.sounds.SilentSound;
-import fridaymario.sounds.Sounds;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.ide.AppLifecycleListener
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.project.ProjectManagerListener
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.text.StringUtilRt
+import fridaymario.listeners.*
+import fridaymario.sounds.SilentSound
+import fridaymario.sounds.Sounds
 
-import java.util.List;
+class IntelliJAppComponent: AppLifecycleListener {
+    private var soundPlayer: ActionListeningSoundPlayer? = null
+    private var disposable: Disposable? = null
+    private var silentMode = false
+    private var logUnmappedActions = false
 
-import static com.intellij.openapi.util.text.StringUtilRt.isEmptyOrSpaces;
+    init {
+        instance = this
+    }
 
-public class IntelliJAppComponent implements AppLifecycleListener {
-	public static IntelliJAppComponent instance;
-	private ActionListeningSoundPlayer soundPlayer;
-	private Disposable disposable;
-	private boolean silentMode;
-	private boolean logUnmappedActions;
+    override fun appFrameCreated(commandLineArgs: List<String>) {
+        if (!Settings.getInstance().isPluginEnabled) return
+        disposable = init()
+    }
 
-	public IntelliJAppComponent() {
-		instance = this;
-	}
+    override fun appWillBeClosed(isRestart: Boolean) {
+        if (!Settings.getInstance().isPluginEnabled) return
+        if (soundPlayer != null && Settings.getInstance().backgroundMusicEnabled) {
+            soundPlayer!!.stopAndPlayGameOver()
+        }
+    }
 
-	@Override public void appFrameCreated(@NotNull List<String> commandLineArgs) {
-		if (!Settings.getInstance().isPluginEnabled()) return;
-		disposable = init();
-	}
+    private fun init(): Disposable {
+        val disposable = Disposer.newDisposable("FridayMario")
+        Disposer.register(ApplicationManager.getApplication(), disposable)
+        soundPlayer = ActionListeningSoundPlayer(createSounds(), createLoggingListener()).init()
+        Disposer.register(disposable, Disposable {
+            if (soundPlayer != null) {
+                soundPlayer!!.stop()
+                soundPlayer = null
+            }
+        })
+        initApplicationListeners(soundPlayer, disposable)
+        initProjectListeners(soundPlayer, disposable)
 
-	@Override public void appWillBeClosed(boolean isRestart) {
-		if (!Settings.getInstance().isPluginEnabled()) return;
-		if (soundPlayer != null && Settings.getInstance().backgroundMusicEnabled) {
-			soundPlayer.stopAndPlayGameOver();
-		}
-	}
+        // see https://github.com/dkandalov/friday-mario/issues/3#issuecomment-160421286
+        // and http://keithp.com/blogs/Java-Sound-on-Linux/
+        val clipProperty = System.getProperty("javax.sound.sampled.Clip")
+        if (SystemInfo.isLinux && clipProperty != null && clipProperty == "org.classpath.icedtea.pulseaudio.PulseAudioMixerProvider") {
+            show("JDK used by your IDE can lock up or fail to play sounds.<br/>" +
+                     "Please see <a href=\"http://keithp.com/blogs/Java-Sound-on-Linux/\">http://keithp.com/blogs/Java-Sound-on-Linux</a> to fix it.")
+        }
+        return disposable
+    }
 
-	private Disposable init() {
-		Disposable disposable = Disposer.newDisposable("FridayMario");
-		Disposer.register(ApplicationManager.getApplication(), disposable);
+    private fun initApplicationListeners(soundPlayer: ActionListeningSoundPlayer?, disposable: Disposable) {
+        AllActions(soundPlayer).start(disposable)
+    }
 
-		soundPlayer = new ActionListeningSoundPlayer(createSounds(), createLoggingListener()).init();
-		Disposer.register(disposable, () -> {
-			if (soundPlayer != null) {
-				soundPlayer.stop();
-				soundPlayer = null;
-			}
-		});
-		initApplicationListeners(soundPlayer, disposable);
-		initProjectListeners(soundPlayer, disposable);
+    private fun initProjectListeners(soundPlayer: ActionListeningSoundPlayer?, disposable: Disposable) {
+        val projectManagerListener: ProjectManagerListener = object: ProjectManagerListener {
+            override fun projectOpened(project: Project) {
+                // TODO create child of project AND disposable?
+                Refactoring(project, soundPlayer).start(project)
+                VcsActions(project, soundPlayer).start(project)
+                Compilation.factory.create(project, soundPlayer).start(project)
+                UnitTests(project, soundPlayer).start(project)
+            }
+        }
 
-		// see https://github.com/dkandalov/friday-mario/issues/3#issuecomment-160421286
-		// and http://keithp.com/blogs/Java-Sound-on-Linux/
-		String clipProperty = System.getProperty("javax.sound.sampled.Clip");
-		if (SystemInfo.isLinux && clipProperty != null && clipProperty.equals("org.classpath.icedtea.pulseaudio.PulseAudioMixerProvider")) {
-			show("JDK used by your IDE can lock up or fail to play sounds.<br/>" +
-				 "Please see <a href=\"http://keithp.com/blogs/Java-Sound-on-Linux/\">http://keithp.com/blogs/Java-Sound-on-Linux</a> to fix it.");
-		}
-		return disposable;
-	}
+        // TODO is this necessary?
+        for (project in ProjectManager.getInstance().openProjects) {
+            projectManagerListener.projectOpened(project)
+        }
+        ProjectManager.getInstance().addProjectManagerListener(projectManagerListener, disposable)
+    }
 
-	private void initApplicationListeners(ActionListeningSoundPlayer soundPlayer, Disposable disposable) {
-		new AllActions(soundPlayer).start(disposable);
-	}
+    fun silentMode(): IntelliJAppComponent {
+        silentMode = true
+        return this
+    }
 
-	private void initProjectListeners(ActionListeningSoundPlayer soundPlayer, Disposable disposable) {
-		ProjectManagerListener projectManagerListener = new ProjectManagerListener() {
-			@Override public void projectOpened(@NotNull Project project) {
-				// TODO create child of project AND disposable?
-				new Refactoring(project, soundPlayer).start(project);
-				new VcsActions(project, soundPlayer).start(project);
-				Compilation.factory.create(project, soundPlayer).start(project);
-				new UnitTests(project, soundPlayer).start(project);
-			}
-		};
+    fun logUnmappedActionsMode(): IntelliJAppComponent {
+        logUnmappedActions = true
+        return this
+    }
 
-		// TODO is this necessary?
-		for (Project project : ProjectManager.getInstance().getOpenProjects()) {
-			projectManagerListener.projectOpened(project);
-		}
+    private fun createSounds(): Sounds {
+        return if (silentMode) {
+            Sounds.createSilent(object: SilentSound.Listener {
+                override fun playing(soundName: String) = show(soundName)
+                override fun stopped(soundName: String) = show("stopped: $soundName")
+            })
+        } else {
+            val settings = Settings.getInstance()
+            Sounds.create(settings.actionSoundsEnabled, settings.backgroundMusicEnabled)
+        }
+    }
 
-		ProjectManager.getInstance().addProjectManagerListener(projectManagerListener, disposable);
-	}
+    private fun createLoggingListener(): ActionListeningSoundPlayer.Listener {
+        return object: ActionListeningSoundPlayer.Listener {
+            override fun unmappedAction(actionId: String) {
+                if (logUnmappedActions) show(actionId)
+            }
 
-	public IntelliJAppComponent silentMode() {
-		silentMode = true;
-		return this;
-	}
+            override fun unmappedRefactoring(refactoringId: String) {
+                if (logUnmappedActions) show(refactoringId)
+            }
+        }
+    }
 
-	public IntelliJAppComponent logUnmappedActionsMode() {
-		logUnmappedActions = true;
-		return this;
-	}
+    fun setBackgroundMusicEnabled(value: Boolean) {
+        Settings.getInstance().setBackgroundMusicEnabled(value)
+        update()
+    }
 
-	private Sounds createSounds() {
-		if (silentMode) {
-			return Sounds.createSilent(new SilentSound.Listener() {
-				@Override public void playing(String soundName) {
-					show(soundName);
-				}
+    fun setActionSoundsEnabled(value: Boolean) {
+        Settings.getInstance().setActionSoundsEnabled(value)
+        update()
+    }
 
-				@Override public void stopped(String soundName) {
-					show("stopped: " + soundName);
-				}
-			});
-		} else {
-			Settings settings = Settings.getInstance();
-			return Sounds.create(settings.actionSoundsEnabled, settings.backgroundMusicEnabled);
-		}
-	}
+    private fun update() {
+        if (disposable != null) disposable!!.dispose()
+        if (Settings.getInstance().isPluginEnabled) disposable = init()
+    }
 
-	private ActionListeningSoundPlayer.Listener createLoggingListener() {
-		return new ActionListeningSoundPlayer.Listener() {
-			@Override public void unmappedAction(String actionId) {
-				if (logUnmappedActions) show(actionId);
-			}
+    companion object {
+        @JvmField
+        var instance: IntelliJAppComponent? = null
 
-			@Override public void unmappedRefactoring(String refactoringId) {
-				if (logUnmappedActions) show(refactoringId);
-			}
-		};
-	}
-
-	private static void show(String message) {
-		if (isEmptyOrSpaces(message)) return;
-		String noTitle = "";
-		Notification notification = new Notification("Friday Mario", noTitle, message, NotificationType.INFORMATION);
-		ApplicationManager.getApplication().getMessageBus().syncPublisher(Notifications.TOPIC).notify(notification);
-	}
-
-	public void setBackgroundMusicEnabled(boolean value) {
-		Settings.getInstance().setBackgroundMusicEnabled(value);
-		update();
-	}
-
-	public void setActionSoundsEnabled(boolean value) {
-		Settings.getInstance().setActionSoundsEnabled(value);
-		update();
-	}
-
-	private void update() {
-		if (disposable != null) disposable.dispose();
-		if (Settings.getInstance().isPluginEnabled()) disposable = init();
-	}
+        private fun show(message: String) {
+            if (StringUtilRt.isEmptyOrSpaces(message)) return
+            val noTitle = ""
+            val notification = Notification("Friday Mario", noTitle, message, NotificationType.INFORMATION)
+            ApplicationManager.getApplication().messageBus.syncPublisher(Notifications.TOPIC).notify(notification)
+        }
+    }
 }
