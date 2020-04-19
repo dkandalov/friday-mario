7a9fa596d8bcbdbd64c001e9d1ee43c1f372bbb2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-04-19 21:27:47 +0100

    don't use deprecated api

--- src/fridaymario/listeners/InitJavaCompilation.kt
+++ src/fridaymario/listeners/InitJavaCompilation.kt
@@ -4,7 +4,7 @@ import com.intellij.ide.AppLifecycleListener
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.compiler.CompilationStatusListener
 import com.intellij.openapi.compiler.CompileContext
-import com.intellij.openapi.compiler.CompilerManager
+import com.intellij.openapi.compiler.CompilerTopics
 import com.intellij.openapi.project.Project
 import fridaymario.util.appMessageBus
 
@@ -16,13 +16,12 @@ class InitJavaCompilation {
                     override fun create(project: Project, listener: Compilation.Listener) =
                         object: Compilation() {
                             override fun start(disposable: Disposable) {
-                                val compilationListener = object: CompilationStatusListener {
+                                project.messageBus.connect(disposable).subscribe(CompilerTopics.COMPILATION_STATUS, object: CompilationStatusListener {
                                     override fun compilationFinished(aborted: Boolean, errors: Int, warnings: Int, compileContext: CompileContext) {
                                         if (errors > 0) listener.compilationFailed()
                                         else listener.compilationSucceeded()
                                     }
-                                }
-                                CompilerManager.getInstance(project).addCompilationStatusListener(compilationListener, disposable)
+                                })
                             }
                         }
                 }
